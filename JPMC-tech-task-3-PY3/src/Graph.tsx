52db2004f3367cb2b4f902c4f4a1f8bee7c4fff1
Parampreet Singh <49701247+param31@users.noreply.github.com>
2020-06-18 05:55:12 +0530

    Add files via upload

---
+++ JPMC-tech-task-3-PY3/src/Graph.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Table } from '@jpmorganchase/perspective';
+import { ServerRespond } from './DataStreamer';
+import { DataManipulator } from './DataManipulator';
+import './Graph.css';
+
+interface IProps {
+  data: ServerRespond[],
+}
+
+interface PerspectiveViewerElement extends HTMLElement {
+  load: (table: Table) => void,
+}
+class Graph extends Component<IProps, {}> {
+  table: Table | undefined;
+
+  render() {
+    return React.createElement('perspective-viewer');
+  }
+
+  componentDidMount() {
+    // Get element from the DOM.
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+
+    const schema = {
+      price_abc: 'float',  //to calculate the ratios
+      price_def: 'float',  //to calculate the ratios
+      ratio: 'float',      //to show the ratio between stocks
+      timestamp: 'date',    // calculation is w.r.t time
+      upper_bound: 'float', //upper threshold
+      lower_bound: 'float',  //lower threshold
+      trigger_alert: 'float', //alert for the correlations 
+    };
+
+    if (window.perspective && window.perspective.worker()) {
+      this.table = window.perspective.worker().table(schema);
+    }
+    if (this.table) {
+      // Load the `table` in the `<perspective-viewer>` DOM reference.
+      elem.load(this.table);
+      elem.setAttribute('view', 'y_line'); //the view
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
+      elem.setAttribute('aggregates', JSON.stringify({
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+      }));
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.table) {
+      this.table.update([
+        DataManipulator.generateRow(this.props.data),
+      ]);
+    }
+  }
+}
+
+export default Graph;
